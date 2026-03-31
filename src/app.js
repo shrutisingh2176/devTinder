32c5f38670e01f295f7c304ff421fb9a133405ee
Shruti Singh <singhshruti2029@gmail.com>
2026-03-31 10:50:04 +0530

    Different routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res) => {
-    res.send("Hello from the server! !!!     ")
-})
+app.use("/hello/2",(req,res)=>{
+    res.send("Abracadabra!");
+});
+
+app.use("/hello",(req,res) => {
+    res.send("Hello hello hello!");
+});
+
+app.use("/test",(req,res)=>{
+    res.send("hello from the server!");
+});
+
+// app.use((req,res) => {
+//     res.send("Nmaste shruti! ");
+// });
 
 
 
-app.listen(3000,()=> {console.log("Server is successfully listening!")});
+app.listen(7777,()=> {
+    console.log("Server is successfully listening!");
+});
