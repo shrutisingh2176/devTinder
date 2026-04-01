c3d7993d372f2749f568ea1c5176a9d8b13839a5
Shruti Singh <singhshruti2029@gmail.com>
2026-04-01 20:05:58 +0530

    isAdminAuthorised

--- src/app.js
+++ src/app.js
@@ -2,29 +2,21 @@
 const express = require("express");
 const app = express();
 
-app.use(
-    "/user",
-    (req,res,next) => {
-        console.log("Handling the route user!!");
-        res.send("response!!")
-        next();
-    },
-(req,res,next) => {
-    console.log("Handling the route user 2 !");
-    res.send("2nd response!!");
-    next();
-},
-(req,res,next) => {
-    console.log("Handling the route user 3 !");
-    res.send("3rd response!!");
-    next();
-},
-(req,res,next) => {
-    console.log("Handling the route user 4 !");
-    res.send("4th response!!");
-    
-}
-);
+app.get("/admin/getAllData",(req,res) => {
+    //Logic of checking if the request is authorised
+    const token ="xyz";
+    const isAdminAuthorised= token ==="xyz";
+    if (isAdminAuthorised) {
+        res.send("All data Sent");
+    } else{
+        res.status(401).send("Unauthorized request");
+    }
+    });
+
+    app.get("/admin/deleteUser", (req,res)=>{
+        //Logic of checking if the request is authorised 
+        res.send("Deleted a user");
+    });
 
 
 app.listen(7777,()=> {
