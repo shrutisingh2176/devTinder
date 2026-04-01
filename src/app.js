2ed268c9a1f97e6e49225561396b31b93a71ccde
Shruti Singh <singhshruti2029@gmail.com>
2026-04-01 17:52:14 +0530

    Multiple routes handlers

--- src/app.js
+++ src/app.js
@@ -2,26 +2,29 @@
 const express = require("express");
 const app = express();
 
-//This will only handle GET call to /user
-app.get("/user",(req,res) => {
-    res.send({ firstName: "Shruti", lastName:"Singh"});
-});
-
-app.post("/user", async(req,res) =>{
-    //saving data to DB 
-    res.send("Data successfully saved")
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted successfully")
-})
-
-app.use("/test",(req,res)=>{
-    res.send("hello from the server!");
-});
-
-
-
+app.use(
+    "/user",
+    (req,res,next) => {
+        console.log("Handling the route user!!");
+        res.send("response!!")
+        next();
+    },
+(req,res,next) => {
+    console.log("Handling the route user 2 !");
+    res.send("2nd response!!");
+    next();
+},
+(req,res,next) => {
+    console.log("Handling the route user 3 !");
+    res.send("3rd response!!");
+    next();
+},
+(req,res,next) => {
+    console.log("Handling the route user 4 !");
+    res.send("4th response!!");
+    
+}
+);
 
 
 app.listen(7777,()=> {
