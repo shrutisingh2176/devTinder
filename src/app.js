47c6edbf74f49e93cd783c2085a8e8a3489a4e57
Shruti Singh <singhshruti2029@gmail.com>
2026-04-03 19:29:45 +0530

    Error Middleware-1

--- src/app.js
+++ src/app.js
@@ -1,24 +1,18 @@
 
 const express = require("express");
 const app = express();
-const {adminAuth , userAuth} = require("./middlewares/auth.js");
 
-app.use("/admin", adminAuth);
 
-app.post("/admin/login",(req,res) =>{
-    res.send("User Logged in successfully!");
-})
-
-app.get("/user" , userAuth,(req,res) =>{
-    res.send("User Data Sent");
-})
-
-app.get("/admin/getAllData",(req,res) => {
-res.send("All Data Sent");
+app.get("/getUserData" ,(req,res) =>{
+   throw new Error ("xyz");
+   res.send("User Data Sent");
 });
 
-app.get("/admin/deleteUser", (req,res)=>{
-    res.send("Deleted a user");
+app.use("/",(err,req,res,next) => {
+if (err) {
+    //log your errors also 
+    res.status(500).send("something went wrong");
+}
 });
 
 
